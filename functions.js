d7e273396c88975ebc09d4a5f0d8aab72813fca9
Josh Mellicker <josh@platformpurple.com>
2019-04-22 22:41:47 -0700

    two query tests passing

--- functions.js
+++ functions.js
@@ -1,11 +1,21 @@
 module.exports = {
     op: {
         description: "accepts a request object, executes and returns a result",
-        action: async(req) => {
-            return 'hi ' + req
+        action: (request) => {
+            let table = request.table
+            let sql
+            switch (request.method) {
+                case 'get':
+                    sql = `SELECT ${ request.fields || '*' } FROM ${ table }`
+                    if (request.where) sql += ` WHERE ${ request.where }`
+                    break
+            
+                default:
+                    break
+            }
+            console.log(sql)
+            return sql
+            
         }
-    },
-    b(){
-        return 'b'
     }
 }
--- test.js
+++ test.js
@@ -1,10 +1,22 @@
 
 const theFunctions = require('./functions')
 
-test('b', () => {
-    expect (theFunctions.b()).toBe('b')
-  })
+test('simple query', () => {
+    expect (theFunctions.op.action({
+        method: 'get',
+        table: 'users',
+        where: `id = 'qqq'`
 
-test('this one', () => {
-    expect (theFunctions.op.action('bob')).toBe('bob')
+    })).toBe(`SELECT * FROM users WHERE id = 'qqq'`)
 })
+
+test('specifying fields', () => {
+    expect (theFunctions.op.action({
+        method: 'get',
+        table: 'users',
+        fields: ['id', 'first_name', 'email'],
+        where: `id = 'qqq'`
+
+    })).toBe(`SELECT id,first_name,email FROM users WHERE id = 'qqq'`)
+})
+
